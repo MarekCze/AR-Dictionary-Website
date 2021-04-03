78e23a2ac080942c8c5b20576001a4a27ee1a0f0
Marek Czerniejewski <marekcze@outlook.com>
2021-04-03 17:18:34 +0100

    added another component, updated css

--- ar-dictionary-website/src/views/Home.vue
+++ ar-dictionary-website/src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <AboutComponent/>
+    <AppGuideComponent/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import AboutComponent from "@/components/AboutComponent.vue";
+import AppGuideComponent from "@/components/AppGuideComponent.vue";
 
 export default {
   name: "Home",
   components: {
     AboutComponent,
+    AppGuideComponent,
   },
 };
 </script>
