822255932c86525242cb94714db8ce00209ad459
MarekCze <marekcze@outlook.com>
2021-04-04 11:46:57 +0100

    homepage is finished

--- ar-dictionary-website/src/components/DownloadAppComponent.vue
+++ ar-dictionary-website/src/components/DownloadAppComponent.vue
@@ -2,5 +2,6 @@
     <div id="download-app-wrapper">
         <img src="@/assets/img/logo/logoBlue.png" alt="logo">
         <h1>Download the app!</h1>
+        <img id ="curved-arrow" src="@/assets/img/svg/curved-right-white-arrow.svg" alt="curved arrow">
     </div>
 </template>
