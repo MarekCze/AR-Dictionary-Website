0267e33209e487c1ae1d519dd02841db6b4f9224
Marek Czerniejewski <marekcze@outlook.com>
2021-05-04 18:46:03 +0100

    added link for apk

--- ar-dictionary-website/src/components/DownloadAppComponent.vue
+++ ar-dictionary-website/src/components/DownloadAppComponent.vue
@@ -1,6 +1,8 @@
 <template>
     <div id="download-app-wrapper">
-        <img src="@/assets/img/logo/logoBlue.png" alt="logo">
+        <a href="@/assets/release/app-release-unsigned.apk">
+            <img src="@/assets/img/logo/logoBlue.png" alt="logo">
+        </a>
         <h1>Download the app!</h1>
         <img id ="curved-arrow" src="@/assets/img/svg/curved-right-white-arrow.svg" alt="curved arrow">
     </div>
